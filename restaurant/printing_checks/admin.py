122c75bb884756e32e5bf70e3396bf20bd38bb04
vitaliirv <vitaliymakovka@gmail.com>
2022-12-17 13:32:18 +0200

    added models and filters for adminpanel

--- restaurant/printing_checks/admin.py
+++ restaurant/printing_checks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Printer, Check
 
-# Register your models here.
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'api_key', 'check_type', 'point_id')
+    list_filter = ('name', 'check_type')
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ('id', 'printer_id', 'type', 'order', 'status', 'pdf_file')
+    list_filter = ('printer_id__name', 'type', 'status')
--- restaurant/printing_checks/views.py
+++ restaurant/printing_checks/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'printing_checks/base.html')
+    return render(request, 'printing_checks/index.html')
